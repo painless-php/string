8645a33acdd72c56bf18a2da1fd60465f47ae80e
Mark Magyar <14284867+xHeaven@users.noreply.github.com>
2023-05-21 22:09:01 +0200

    replace function cast with type cast

--- src/Conversion/StringToFloatTypeConverter.php
+++ src/Conversion/StringToFloatTypeConverter.php
@@ -7,10 +7,10 @@ use PainlessPHP\String\Exception\StringTypeConversionException;
 
 class StringToFloatTypeConverter implements StringTypeConverterInterface
 {
-    public function convert(string $value) : float
+    public function convert(string $value): float
     {
-        if(is_numeric($value)) {
-            return floatval($value);
+        if (is_numeric($value)) {
+            return (float)$value;
         }
 
         throw StringTypeConversionException::fromConversion($value, $this);
